8644e85a7a25dd52a4aacff92ee6fe62c327ee18
Sean Grady <scgrady@smu.edu>
2024-04-24 11:07:00 -0500

    modified:   app/signin/page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -51,7 +51,7 @@ const SignIn = () => {
             <div className="text-center">
               <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign in</h1>
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                Don't have an account yet?
+                Don&apos;t have an account yet?
                 <a className="text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/signup">
                   &nbsp;Sign up here
                 </a>
